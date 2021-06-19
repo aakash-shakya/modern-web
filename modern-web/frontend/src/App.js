a86113dec470f3d4105bfa822516a77ef33c87e7
aakash shakya <official.aakas@gmail.com>
2021-06-19 10:15:55 -0700

    navigation with routers
    used react-routers to navigate between the components

--- modern-web/frontend/src/App.js
+++ modern-web/frontend/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import FetchData from './components/fetchdata';
 import Postdata from './components/postData';
+import ComponentRoute from './components/componentroute';
 
 function App() {
   return (
     <div className="App">
-      <Postdata/>
-      <FetchData/>
+      <h3>
+        <ComponentRoute/>
+      </h3>
     </div>
   );
 }
--- modern-web/frontend/src/components/componentroute.js
+++ modern-web/frontend/src/components/componentroute.js
@@ -1,1 +1,27 @@
 import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Switch,Link} from 'react-router-dom';
+import postData from './postData';
+import FetchData from './fetchdata'
+
+class RouteComponent extends Component{
+    render(){
+        return(
+        <div>
+            <Router>
+                <ul>
+                    <li><Link to="/">Post Data</Link></li>
+                    <li><Link to="/post">Post</Link></li>
+                </ul>
+
+                <Switch>
+                    <Route exact path="/" component={FetchData}/>
+                    <Route path="/post" component={postData}/>
+                </Switch>
+            </Router>
+        </div>
+        )
+    }
+}
+
+
+export default RouteComponent;
