92f07d3db62743583550d67e157a26a1b796766f
aakash shakya <official.aakas@gmail.com>
2021-06-19 09:52:13 -0700

    class based components in reactjs
    api hit 1 time only than in function based components

--- modern-web/frontend/src/App.js
+++ modern-web/frontend/src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Fetchdata from './components/fetchdata';
+import FetchData from './components/fetchdata';
 import Postdata from './components/postData';
 
 function App() {
   return (
     <div className="App">
       <Postdata/>
-      <Fetchdata/>
+      <FetchData/>
     </div>
   );
 }
--- modern-web/frontend/src/components/fetchdata.js
+++ modern-web/frontend/src/components/fetchdata.js
@@ -1,4 +1,7 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, Component} from 'react';
+
+
+// FUNCTION BASED COMPONENT
 
 const Fetchdata= () =>{
     const [data, setData] = useState(null);
@@ -44,4 +47,86 @@ const Fetchdata= () =>{
     )
 }
 
-export default Fetchdata;
+
+// CLASS BASED COMPONENT
+
+
+class FetchData extends Component{
+    constructor(props){
+        super(props)
+        this.state ={
+            data:[],
+            loading:true,
+            error:null,
+        };
+    }
+
+    componentDidMount(){
+        const url = 'http://localhost:8000/get/';
+
+        const getDatas = () => 
+        fetch(url).then(response=>{
+                if(response.ok){
+                    return response.json()
+                }
+                else{
+                    this.setState({error:response.error(),loading:false})
+                }
+            });
+        
+
+        getDatas().then(data => this.setState({data:data, loading:false}))
+                 .catch(error=>this.setState({error:error,loading:false}));
+    }
+
+    render(){
+
+        const { data, loading, error } = this.state;
+
+        if (error){
+            return(
+                <div>
+                    <h1>Error occured</h1>
+                </div>
+            )
+        }
+
+        return(
+            <div>
+
+        { loading ? <h1>Loading</h1> :
+            <div className="container">
+                <div className="tableFixHead">
+                <table className="table table-striped table-light table-bordered">
+                    <thead className="table-dark">
+                        <tr>
+                            <th>Year</th>
+                            <th>Product</th>
+                            <th>Sale</th>
+                            <th>Country</th>
+                        </tr>
+                    </thead>
+            { data?.map((item)=>
+                    <tbody>
+                        <tr>
+                            <td key={item.id}>{item.year}</td>
+                            <td>{item.product}</td>
+                            <td>{item.sale}</td>
+                            <td>{item.country}</td>
+                        </tr>
+                    </tbody>
+        
+                )}
+                </table>
+            </div>
+            </div>
+        }
+
+        </div>
+            
+        )
+            
+    }
+}
+
+export default FetchData;
